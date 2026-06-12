f05e877184fe8aa280a671f6860726ad72073a05
beadevo <bealingual@gmail.com>
2026-06-12 14:42:48 +0800

    add bedev's file

---
+++ GitTutorial/bedevScreen.swift
@@ -0,0 +1,18 @@
+//
+//  bedevScreen.swift
+//  GitTutorial
+//
+//  Created by Beatrice Deviana on 12/06/26.
+//
+
+import SwiftUI
+
+struct bedevScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    bedevScreen()
+}
