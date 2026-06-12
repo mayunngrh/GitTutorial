f706aa920dd78f65bfd6e6482d62860d2c3ec078
Mayun Suryatama <mayunngrh@gmail.com>
2026-06-12 10:12:27 +0800

    Initial Commit

---
+++ GitTutorial/GitTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTutorialApp.swift
+//  GitTutorial
+//
+//  Created by Mayun Suryatama on 12/06/26.
+//
+
+import SwiftUI
+
+@main
+struct GitTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
